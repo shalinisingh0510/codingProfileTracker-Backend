1d24121b51b800addebb98320978a793e5adea52
shalini <shalinishareyasinghl@gmail.com>
2026-05-23 16:23:20 +0530

    feat: add Google Sign-In support and verify endpoint

--- codeprofile-aggregator/backend/routes/authRoutes.js
+++ codeprofile-aggregator/backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, forgotPassword, resetPassword } = require('../controllers/authController');
+const { registerUser, loginUser, forgotPassword, resetPassword, googleLogin } = require('../controllers/authController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', resetPassword);
+router.post('/google', googleLogin);
 
 module.exports = router;
